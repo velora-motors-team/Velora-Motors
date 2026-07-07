1570b1c5be8f089f189a50b99dde10af465d5073
7madh39 <s12324033@stu.najah.edu>
2026-07-07 16:21:11 +0300

    hmada is a loard

--- src/main/java/com/velora/Velora_Motors.java
+++ src/main/java/com/velora/Velora_Motors.java
@@ -1,28 +1,23 @@
-//package com.velora;
-//
-//import com.velora.ui.LoginScreen;
-//import javax.swing.SwingUtilities;
-//import javax.swing.UIManager;
-//
-//public class Velora_Motors {
-//
-//    public static void main(String[] args) {
-//        SwingUtilities.invokeLater(() -> {
-//            try {
-//                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-//            } catch (Exception ignored) {
-//            }
-//
-//            LoginScreen screen = new LoginScreen();
-//            screen.setVisible(true);
-//            
-//            
-//            
-//        });
-//    }
-//}
-
 package com.velora;
+
+import com.velora.ui.LoginScreen;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+public class Velora_Motors {
+
+   public static void main(String[] args) {
+      SwingUtilities.invokeLater(() -> {
+          try {             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+           } catch (Exception ignored) {
+          }
+
+           LoginScreen screen = new LoginScreen();
+           screen.setVisible(true);
+       });
+    }
+}
+
+/*package com.velora;
 
 import com.velora.authentication.Customer;
 import com.velora.ui.CustomerDashboard;
@@ -49,4 +44,4 @@ public class Velora_Motors {
             screen.setVisible(true);
         });
     }
-}
+}*/
